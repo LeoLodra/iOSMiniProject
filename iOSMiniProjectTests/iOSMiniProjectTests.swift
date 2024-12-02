99b9b87f7f8f46a74434591083f13825746bc7ca
LeoLodra <leonardomarhan5@gmail.com>
2024-12-02 14:35:23 +0700

    Initial Commit

---
+++ iOSMiniProjectTests/iOSMiniProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMiniProjectTests.swift
+//  iOSMiniProjectTests
+//
+//  Created by Leonardo Marhan on 02/12/24.
+//
+
+import Testing
+@testable import iOSMiniProject
+
+struct iOSMiniProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
